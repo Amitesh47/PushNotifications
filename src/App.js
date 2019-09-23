fddda37ebadbba24a9dd9d4ef4118dfb45942b05
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-09-23 15:27:14 +0530

    Poc for push notification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import { isPushNotificationSupported } from "./push-notification"
+import { isPushNotificationSupported } from "./pushNotifications"
+import PushNotificationsComponent from "./PushNotificationsComponent"
 
 function App() {
   console.log(isPushNotificationSupported());
   return (
     <div className="App">
-      
+      <PushNotificationsComponent/>
     </div>
   );
 }
