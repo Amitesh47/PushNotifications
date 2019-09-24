8a9b68777f51579013bffe936670819dbfaa6725
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-09-24 15:13:01 +0530

    Minor changes

--- push-server/src/app.js
+++ push-server/src/app.js
@@ -9,28 +9,13 @@ const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
-// var corsOptions = {
-//   origin: 'http://localhost:8080',
-// }
-
-// // cors.origin = true;
-// app.options('*', cors()) 
-
-
 app.use(
   cors({
-    // origin(origin, cb) {
-    //   const whitelist = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(",") : [];
-    //   cb(null, whitelist.includes(origin));
-    // },
     credentials: true, 
-    origin: "https://localhost:3000",
     methods: ['GET','POST'],
     allowedHeaders: ['content-type','sec-fetch-mode','origin']
   })
 );
-
-// app.use(cors({origin: 'https://localhost:3000'}));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
